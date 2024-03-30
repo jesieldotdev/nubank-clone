39855fa049abb82e18db81805500d0daffcbe03d
jesieldotdev <jesiel364@gmail.com>
2024-03-29 23:48:41 -0400

    feat: responsible

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 // import './App.css'
+import React from 'react'
 import { AppBar } from './components/AppBar'
 import { BankWallet } from './components/BankWallet'
 import { BankWalletDesktop } from './components/BankWalletDesktop'
@@ -11,9 +12,26 @@ import { MyCard } from './components/MyCard'
 
 function App() {
 
+  const [isMobile, setIsMobile] = React.useState(false);
+
+  React.useEffect(() => {
+      const checkMobile = () => {
+          setIsMobile(window.innerWidth <= 768); // 768px é um breakpoint comum para dispositivos móveis
+      };
+
+      // Verifica a largura da janela quando o componente é montado
+      checkMobile();
+
+      // Atualiza o estado quando a janela é redimensionada
+      window.addEventListener('resize', checkMobile);
+
+      // Limpa o evento quando o componente é desmontado
+      return () => window.removeEventListener('resize', checkMobile);
+  }, []);
+
   return (
     <div>
-      {true ? <>
+      {isMobile ? <>
         <div className='bg-primary px-6 py-2'>
           <AppBar />
           <CardMessages />
