e9815735f1d2b78064f71aea4a448dc829544ff6
jesieldotdev <jesiel364@gmail.com>
2024-04-03 16:47:59 -0400

    feat

--- src/pages/Desktop/components/HeaderDesktop/index.tsx
+++ src/pages/Desktop/components/HeaderDesktop/index.tsx
@@ -3,10 +3,13 @@ import Logo from '../../../../assets/nubanklogo.svg'
 
 
 export const HeaderDesktop = () => {
+  
     return (
-        <div className='flex justify-between px-12 py-3 text-xs items-center bg-[#f7f7ff]'>
-            <img src={Logo} className='w-10 h-10'/>
-            <button className='bg-[#efefef] px-7 py-2 rounded-full  text-zinc-800 font-semibold text-xs'>Desconectar</button>
+        <div
+
+            className={`flex justify-between px-12 py-3 text-xs items-center bg-[#f7f7ff] `}>
+            <img src={Logo} className='w-10 h-10' />
+            <button  className='bg-[#efefef] px-7 py-2 rounded-full  text-zinc-800 font-semibold text-xs'>Desconectar</button>
         </div>
     )
 }
